354b041bc15c035ceb00738dfdf7379a2806bdd8
JGluhov <jgluhov@gmail.com>
2016-01-11 14:41:02 +0300

    Commit useless code

--- src/ts/app.component.ts
+++ src/ts/app.component.ts
@@ -2,14 +2,14 @@
  * Created by jgluhov on 07/01/16.
  */
 import {Component, View} from 'angular2/core';
-import {TodoComponent} from './todo/components/todo.component';
+//import {TodoComponent} from './todo/components/todo.component';
 
 @Component({
     selector: 'app'
 })
 
 @View({
-    directives: [TodoComponent],
+    directives: [/*TodoComponent*/],
     template: `
     <h1>The best programmer is {{name}}</h1>
     `
