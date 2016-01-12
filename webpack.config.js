0eb4a437a0553b6b2ac7623c93425707ce5aa842
JGluhov <jgluhov@gmail.com>
2016-01-12 16:17:52 +0300

    add fonts ubuntu to project

--- webpack.config.js
+++ webpack.config.js
@@ -16,39 +16,31 @@ module.exports = {
     filename: '[name].js'
   },
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js', '.jade']
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js', '.jade', '.ttf'],
+    modulesDirectories: ['node_modules', 'bower_components']
   },
   devtool: 'inline-source-map',
   watch: true,
-  watchOptions: {
-    aggregateTimeout: 300
-  },
+  watchOptions: { aggregateTimeout: 300 },
   plugins: [
     new CopyWebpackPlugin([ { from: 'src/assets', to: 'assets' } ]),
     new ExtractTextPlugin("styles.css", {disable: process.env.NODE_ENV == 'development'}),
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
-    loaders: [
-      {
+    loaders: [{
         test: /\.ts$/,
         loader: 'ts',
-        query: {
-          'ignoreDiagnostics': [
-            2307 // 2307 -> Cannot find module [*.jade]
-          ]
-        },
-        exclude: /node_modules/
-      },
-      {
+        query: { 'ignoreDiagnostics': [ 2307 /* 2307 -> Cannot find module [*.jade] */ ]}
+      }, {
         test: /\.styl$/,
-        loader: ExtractTextPlugin.extract("style", "css!stylus"),
-        exclude: /node_modules/
-      },
-      {
+        loader: ExtractTextPlugin.extract('style', 'css!stylus?resolve url')
+      }, {
         test: /\.jade$/,
-        loader: 'jade',
-        exclude: /node_modules/
+        loader: 'jade'
+      }, {
+        test: /\.ttf$/,
+        loader: 'file?name=[path][name].[ext]'
       }
     ]
   },
